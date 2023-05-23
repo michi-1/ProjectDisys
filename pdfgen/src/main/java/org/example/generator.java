8c11c2beba4e52dea9164f82d809e145cd342a03
michi-1 <reiter.michael@orotonmail.com>
2023-05-23 14:41:24 +0200

    pdfgen

---
+++ pdfgen/src/main/java/org/example/generator.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class generator {
+    public void generate() throws FileNotFoundException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+
+        document.add(chunk);
+        document.close();
+    }
+}
